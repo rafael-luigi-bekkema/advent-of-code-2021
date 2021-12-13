ede823c975771faf7dd3ceedd6198e9f313749fb
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2021-12-13 13:14:12 +0100

    Day 13

---
+++ day13.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+type coord struct {
+	x, y int
+}
+
+func day13a(input io.Reader, foldLimit int, ascii io.Writer) int {
+	s := bufio.NewScanner(input)
+	grid := make(map[coord]bool)
+	var maxx, maxy int
+	for s.Scan() {
+		line := s.Text()
+		if line == "" {
+			break // no more dots
+		}
+		var x, y int
+		fmt.Sscanf(line, "%d,%d", &x, &y)
+		grid[coord{x, y}] = true
+		if x > maxx {
+			maxx = x
+		}
+		if y > maxy {
+			maxy = y
+		}
+	}
+
+	// instructions
+	var foldCount int
+	for s.Scan() {
+		foldCount++
+		var axis string
+		var along int
+		fmt.Sscanf(s.Text(), "fold along %1s=%d", &axis, &along)
+
+		if axis == "x" {
+			for curx := maxx; curx > along; curx-- {
+				for cury := 0; cury <= maxy; cury++ {
+					key := coord{curx, cury}
+					_, ok := grid[key]
+					if !ok {
+						continue
+					}
+					delete(grid, key)
+					key.x = along - (key.x - along)
+					grid[key] = true
+				}
+			}
+			maxx = along - 1
+		}
+		if axis == "y" {
+			for cury := maxy; cury > along; cury-- {
+				for curx := 0; curx <= maxx; curx++ {
+					key := coord{curx, cury}
+					_, ok := grid[key]
+					if !ok {
+						continue
+					}
+					delete(grid, key)
+					key.y = along - (key.y - along)
+					grid[key] = true
+				}
+			}
+			maxy = along - 1
+		}
+		if foldLimit > 0 && foldCount >= foldLimit {
+			break
+		}
+	}
+	if ascii != nil {
+		for cury := 0; cury <= maxy; cury++ {
+			for curx := 0; curx <= maxx; curx++ {
+				if grid[coord{curx, cury}] {
+					fmt.Fprint(ascii, "#")
+				} else {
+					fmt.Fprint(ascii, ".")
+				}
+			}
+			fmt.Fprintln(ascii)
+		}
+	}
+	return len(grid)
+}
+
+func Day13a() {
+	f, err := os.Open("input/day13.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	result := day13a(f, 1, nil)
+	fmt.Printf("day 13a: %d\n", result)
+}
+
+func Day13b() {
+	f, err := os.Open("input/day13.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	var s strings.Builder
+	day13a(f, -1, &s)
+	fmt.Println(s.String())
+}
---
+++ day13_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestDay13a(t *testing.T) {
+	input := `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`
+	expect := 17
+	result := day13a(strings.NewReader(input), 1, nil)
+	if result != expect {
+		t.Fatalf("expected %d, got %d", expect, result)
+	}
+}
+
+func ExampleDay13a() {
+	Day13a()
+	// Output: day 13a: 729
+}
+
+func TestDay13b(t *testing.T) {
+	input := `6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5`
+	var out strings.Builder
+	day13a(strings.NewReader(input), 10, &out)
+	result := out.String()
+	expect := "#####\n" +
+		"#...#\n" +
+		"#...#\n" +
+		"#...#\n" +
+		"#####\n" +
+		".....\n" +
+		".....\n"
+	if result != expect {
+		t.Fatalf("expected\n%s, got\n%s", expect, result)
+	}
+}
+
+func ExampleDay13b() {
+	Day13b()
+
+	// Output:
+	// ###...##..####.#....###..#..#.####.###..
+	// #..#.#..#....#.#....#..#.#..#.#....#..#.
+	// #..#.#......#..#....###..####.###..#..#.
+	// ###..#.##..#...#....#..#.#..#.#....###..
+	// #.#..#..#.#....#....#..#.#..#.#....#....
+	// #..#..###.####.####.###..#..#.#....#....
+}
