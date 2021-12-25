450aeec01f03a81701a8cb787c812ecc297e7f1e
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2021-12-25 14:33:38 +0100

    Last day done. We did it, 50 stars :-)

--- day23.go
+++ day23.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"strings"
 )
 
-var energyM = map[byte]int{
-	'A': 1,
-	'B': 10,
-	'C': 100,
-	'D': 1000,
-}
-
-func energyPer(c byte) int {
-	if v, ok := energyM[c]; ok {
-		return v
+func energyPer(p *Point) int {
+	switch p.letter {
+	case 'A':
+		return 1
+	case 'B':
+		return 10
+	case 'C':
+		return 100
+	case 'D':
+		return 1000
+	default:
+		panic("unknown char")
 	}
-	panic("unknown char")
 }
 
 func slot(c byte) [2]int {
@@ -36,59 +36,51 @@ func slot(c byte) [2]int {
 	}
 }
 
-func home(grid []byte, i int) bool {
-	s := slot(grid[i])
-	if s[1] == i {
-		return true
-	}
-	return s[0] == i && grid[s[1]] == grid[i]
-}
-
-func targetFree(grid []byte, c byte) (int, bool) {
-	s := slot(c)
-	if grid[s[0]] == '.' {
-		if grid[s[1]] == '.' {
-			return s[1], true
+func (grid *Grid) targetFree(point *Point) (int, bool) {
+	for i := grid.height - 1; i >= 1; i-- {
+		idx := i*grid.width + point.homeCol
+		tp := grid.data[idx]
+		if tp == nil {
+			return idx, true
 		}
-		if grid[s[1]] != byte(c) {
+		if tp.letter != point.letter {
 			return 0, false
 		}
-		return s[0], true
 	}
 	return 0, false
 }
 
-func havePath(grid []byte, i, j int, c byte, energy int) (bool, int) {
+func (grid *Grid) havePath(i, j int, p *Point, energy int) (bool, int) {
 	if i == j {
-		return grid[j] == '.', energy
+		return grid.data[j] == nil, energy
 	}
-	rowi := i / 11
-	coli := i % 11
-	rowj := j / 11
-	colj := j % 11
-	if rowi > 0 && rowj < rowi { // move up
-		if grid[i-11] != '.' {
+	rowi := i / grid.width
+	coli := i % grid.width
+	rowj := j / grid.width
+	colj := j % grid.width
+	if rowi > 0 && colj != coli { // move up
+		if grid.data[i-grid.width] != nil {
 			return false, 0
 		}
-		return havePath(grid, i-11, j, c, energy+energyPer(c))
+		return grid.havePath(i-grid.width, j, p, energy+energyPer(p))
 	}
 	if coli == colj && rowj > rowi { // j is below
-		if grid[i+11] != '.' {
+		if grid.data[i+grid.width] != nil {
 			return false, 0
 		}
-		return havePath(grid, i+11, j, c, energy+energyPer(c))
+		return grid.havePath(i+11, j, p, energy+energyPer(p))
 	}
 
 	if colj > coli {
-		if grid[i+1] != '.' {
+		if grid.data[i+1] != nil {
 			return false, 0
 		}
-		return havePath(grid, i+1, j, c, energy+energyPer(c))
+		return grid.havePath(i+1, j, p, energy+energyPer(p))
 	} else {
-		if grid[i-1] != '.' {
+		if grid.data[i-1] != nil {
 			return false, 0
 		}
-		return havePath(grid, i-1, j, c, energy+energyPer(c))
+		return grid.havePath(i-1, j, p, energy+energyPer(p))
 	}
 }
 
@@ -96,15 +88,6 @@ func copyGrid(g []byte) []byte {
 	grid := make([]byte, len(g))
 	copy(grid, g)
 	return grid
-}
-
-func isDone(grid []byte) bool {
-	for _, c := range []byte{'A', 'B', 'C', 'D'} {
-		if s := slot(c); grid[s[0]] != c || grid[s[1]] != c {
-			return false
-		}
-	}
-	return true
 }
 
 /*
@@ -115,47 +98,45 @@ func isDone(grid []byte) bool {
   #########
 */
 
-func move(grid []byte, skips []int, energy int, out chan int, mine *int) {
-	// renderGrid(grid)
-	// fmt.Println("energy", energy)
-	// time.Sleep(time.Second * 5)
+func (grid Grid) move(skips []int, energy int, out chan int, mine *int) {
+	if *mine > 0 && energy >= *mine {
+		// fmt.Println("skip too high")
+		return
+	}
 
-	if isDone(grid) {
+	// grid.render()
+	// fmt.Println("energy", energy)
+	// time.Sleep(time.Second * 1)
+
+	if grid.isDone() {
 		if *mine == 0 || energy < *mine {
 			out <- energy
 			*mine = energy
 		}
-	}
-
-	if *mine > 0 && energy >= *mine {
-		// fmt.Println("skip too high")
-		return
 	}
 	// if *mine <= 13539 {
 	// 	renderGrid(grid)
 	// 	fmt.Println("energy", energy)
 	// }
 outer:
-	for i := range grid {
+	for i, point := range grid.data {
+		if point == nil || grid.homes[point.idx] {
+			continue
+		}
 		for _, s := range skips {
 			if i == s {
 				continue outer
 			}
 		}
-		if isletter := 'A' <= grid[i] && grid[i] <= 'D'; !isletter || home(grid, i) {
-			continue
-		}
-		if j, ok := targetFree(grid, grid[i]); ok {
-			if ok, e := havePath(grid, i, j, grid[i], 0); ok {
-				grid := copyGrid(grid)
-				grid[j] = grid[i]
-				grid[i] = '.'
-				// if *mine <= 10625 {
-				// 	fmt.Printf("target %d -> %d: %c %v %v\n", i, j, grid[j], energy+e, *mine)
-				// 	renderGrid(grid)
-				// }
-				move(grid, nil, energy+e, out, mine)
-				if *mine <= energy {
+		if j, ok := grid.targetFree(point); ok {
+			if ok, e := grid.havePath(i, j, point, 0); ok {
+				grid := grid
+				grid.data[j] = grid.data[i]
+				grid.data[i] = nil
+				grid.homes[point.idx] = true
+				// fmt.Printf("target %d -> %d: %c %v %v\n", i, j, point.letter, energy+e, *mine)
+				grid.move(nil, energy+e, out, mine)
+				if *mine > 0 && *mine <= energy {
 					continue outer
 				}
 				continue outer
@@ -164,7 +145,7 @@ outer:
 		inhall := 0 <= i && i < 11
 		if !inhall { // in hallway
 			// if someone above, skip for now
-			if i > 22 && grid[i-11] != '.' {
+			if i > 22 && grid.data[i-11] != nil {
 				continue
 			}
 			// if i > 22 {
@@ -175,16 +156,16 @@ outer:
 			// }
 			// try all hallway positions, and no move at all
 			for _, h := range []int{1, 3, 5, 7, 9, 10, 0} {
-				if ok, e := havePath(grid, i, h, grid[i], 0); ok {
-					grid := copyGrid(grid)
-					grid[h] = grid[i]
-					grid[i] = '.'
+				if ok, e := grid.havePath(i, h, point, 0); ok {
+					grid := grid
+					grid.data[h] = grid.data[i]
+					grid.data[i] = nil
 					// if *mine <= 10625 {
 					// 	fmt.Printf("hall %d -> %d: %c %v %v\n", i, h, grid[h], energy+e, *mine)
 					// 	renderGrid(grid)
 					// }
-					move(grid, nil, energy+e, out, mine)
-					if *mine <= energy {
+					grid.move(nil, energy+e, out, mine)
+					if *mine > 0 && *mine <= energy {
 						continue outer
 					}
 				}
@@ -197,62 +178,130 @@ outer:
 	}
 }
 
-func renderGrid(grid []byte) {
-	width := 11
-	fmt.Println(strings.Repeat("#", width+2))
+func (grid *Grid) render() {
+	fmt.Println(strings.Repeat("#", grid.width+2))
 	fmt.Print("#")
-	for _, g := range grid[:width] {
-		fmt.Printf("%c", g)
+	for _, g := range grid.data[:grid.width] {
+		var l byte
+		if g == nil {
+			l = '.'
+		} else {
+			l = g.letter
+		}
+		fmt.Printf("%c", l)
 	}
 	fmt.Print("#")
-	for i, g := range grid[width:] {
-		if i%width == 0 {
+	for i, g := range grid.data[grid.width : grid.height*grid.width] {
+		if i%grid.width == 0 {
 			if i != 0 {
 				fmt.Print(" ")
 			}
 			fmt.Print("\n ")
 		}
-		fmt.Printf("%c", g)
+		var l byte
+		if g == nil {
+			if i%grid.width%2 == 1 {
+				l = '#'
+			} else {
+				l = '.'
+			}
+		} else {
+			if grid.homes[g.idx] {
+				l = g.letter + ('h' - 'H')
+			} else {
+				l = g.letter
+			}
+		}
+		fmt.Printf("%c", l)
 	}
 	fmt.Print(" \n  ")
-	fmt.Println(strings.Repeat("#", width-2))
+	fmt.Println(strings.Repeat("#", grid.width-2))
 }
 
-func day23a(input string) int {
-	width := 11
-	data := strings.Split(input, "\n")
-	grid := bytes.Repeat([]byte{'#'}, width*3)
-	for i := 0; i < 11; i++ {
-		grid[i] = '.'
-	}
-	for i := 2; i <= 8; i += 2 {
-		grid[1*width+i] = data[2][i+1]
-		grid[2*width+i] = data[3][i+1]
-	}
-	renderGrid(grid)
+func day23a(data []string) int {
+	grid := day23grid(data)
+	// grid.render()
 	out := make(chan int)
 	go func() {
 		defer close(out)
 		var mine int
-		move(grid, nil, 0, out, &mine)
+		grid.move(nil, 0, out, &mine)
 		fmt.Println("done!")
 	}()
 	var minval int
 	for val := range out {
 		if minval == 0 || val < minval {
 			minval = val
-			fmt.Println(val)
+			// fmt.Println(val)
 		}
 	}
 	return minval
 }
 
-func day23file() string {
+type Grid struct {
+	width, height int
+	data          [55]*Point
+	homes         [16]bool
+}
+
+type Point struct {
+	letter  byte
+	homeCol int
+	idx     int
+}
+
+func (grid *Grid) home(i int, p *Point) bool {
+	if i < grid.width {
+		return false
+	}
+	if i%grid.width == p.homeCol {
+		for row := grid.height - 1; row > (i / grid.width); row-- {
+			if p2 := grid.data[row*grid.width+p.homeCol]; p2 == nil || p2.letter != p.letter {
+				return false
+			}
+		}
+		return true
+	}
+	return false
+}
+
+func (grid *Grid) isDone() bool {
+	for i := 0; i < (grid.height-1)*4; i++ {
+		if !grid.homes[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func day23grid(data []string) Grid {
+	grid := Grid{width: 11, height: len(data) - 2}
+
+	w := grid.width
+	var pidx int
+	for i := 2; i <= 8; i += 2 {
+		for j := 1; j <= grid.height-1; j++ {
+			l := data[j+1][i+1]
+			hcol := int((l - 'A' + 1) * 2)
+			grid.data[j*w+i] = &Point{letter: l, homeCol: hcol, idx: pidx}
+			pidx++
+		}
+	}
+	for i, d := range grid.data {
+		if d == nil {
+			continue
+		}
+		grid.homes[d.idx] = grid.home(i, d)
+	}
+	return grid
+}
+
+func day23file() []string {
 	data, err := os.ReadFile("input/day23.txt")
 	if err != nil {
 		panic(err)
 	}
-	return string(data)
+	return strings.Split(strings.TrimRight(string(data), "\n"), "\n")
 }
 
 func Day23a() {
--- day23_test.go
+++ day23_test.go
@@ -1,30 +1,55 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
-// func TestDay23a(t *testing.T) {
-// 	input := "#############\n" +
-// 		"#...........#\n" +
-// 		"###B#C#B#D###\n" +
-// 		"  #A#D#C#A#\n" +
-// 		"  #########\n"
-// 	expect := 12521
-// 	result := day23a(input)
-// 	if result != expect {
-// 		t.Fatalf("expected %d, got %d", expect, result)
-// 	}
-// }
+func TestDay23a(t *testing.T) {
+	input := "#############\n" +
+		"#...........#\n" +
+		"###B#C#B#D###\n" +
+		"  #A#D#C#A#\n" +
+		"  #########\n"
+	expect := 12521
+	result := day23a(strings.Split(strings.TrimRight(input, "\n"), "\n"))
+	if result != expect {
+		t.Fatalf("expected %d, got %d", expect, result)
+	}
+}
 
 func TestDay23a2(t *testing.T) {
 	input := day23file()
-	expect := 12521
+	expect := 10607
 	result := day23a(input)
 	if result != expect {
 		t.Fatalf("expected %d, got %d", expect, result)
 	}
 }
 
-// func ExampleDay23a() {
-// 	Day23a()
-// 	// Output: day 23a: 0
-// }
+func TestDay23b(t *testing.T) {
+	input := "#############\n" +
+		"#...........#\n" +
+		"###B#C#B#D###\n" +
+		"  #D#C#B#A#\n" +
+		"  #D#B#A#C#\n" +
+		"  #A#D#C#A#\n" +
+		"  #########\n"
+	expect := 44169
+	result := day23a(strings.Split(strings.TrimRight(input, "\n"), "\n"))
+	if result != expect {
+		t.Fatalf("expected %d, got %d", expect, result)
+	}
+}
+
+func TestDay23b2(t *testing.T) {
+	input := day23file()
+	input = append(input, "  #D#C#B#A#", "  #D#B#A#C#")
+	input[5], input[3] = input[3], input[5]
+	input[6], input[4] = input[4], input[6]
+	expect := 59071
+	result := day23a(input)
+	if result != expect {
+		t.Fatalf("expected %d, got %d", expect, result)
+	}
+}
